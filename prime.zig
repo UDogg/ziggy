9b8cf65b585ac019bb84c98668b38f48f6d9fe98
UDogg <choudharyu2003@gmail.com>
2023-08-14 21:47:56 +0530

    seuperfast prime number generator in zig

--- prime.zig
+++ prime.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
+const time = @import("std").time;
+const fs = @import("std").fs;
+const stdin = std.io.getStdIn().reader();
 
 pub fn isPrime(n: u32) bool {
     if (n <= 1) return false;
@@ -32,7 +35,19 @@ pub fn primes(n: u32) void {
 }
 
 pub fn main() !void {
-    const N: u32 = 10;
-    std.debug.print("First {} prime numbers are: ", .{N});
-    primes(N);
+    var input_buffer: [4096]u8 = undefined;
+    const start = time.milliTimestamp();
+    std.debug.print("Enter the number of prime numbers you want to print: ", .{});
+    const input_result = try stdin.readUntilDelimiterOrEof(&input_buffer, '\n');
+    if (input_result) |input| {
+        var n: u32 = try std.fmt.parseInt(u32, std.mem.trim(u8, input, " \n\r\t"), 10);
+        std.debug.print("First {} prime numbers are: ", .{n});
+        primes(n);
+    } else {
+        std.debug.print("Invalid input\n", .{});
+        return;
+    }
+    const end = time.milliTimestamp();
+    const elapsedMillis = end - start;
+    std.debug.print("Execution time(depends on your typing speed and time you took to enter numbers): {} ms\n", .{elapsedMillis});
 }
